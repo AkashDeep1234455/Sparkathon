3553d5124d5fe81f0d471f1323fa68f3234ecfa8
AkashDeep1234455 <akashdeep19735@gmail.com>
2024-08-10 19:11:08 +0530

    nav bar complete

---
+++ frontend/src/NavBar.jsx
@@ -0,0 +1,26 @@
+import "./navBar.css";
+function NavBar() {
+    return ( 
+        <div>
+        <div className="navBar">
+            <div className="search-container">
+                <input type="text" name="search" placeholder="Search..." className="search-input"/>
+                <a href="#" className="search-btn">
+                <i className="fas fa-search"></i>      
+                </a>
+            </div>
+            <div className="navBar-notiNewStock">
+                <div className="navBar-newStock">
+                    <button className="newStock-button">New Stock</button>
+                </div>
+                <div className="navBar-notificaton">
+                <i className="fa-regular fa-bell"></i>
+                </div>
+            </div>
+        </div>
+        <div class="navbar-spacer"></div>
+        </div>
+     );
+}
+
+export default NavBar;
