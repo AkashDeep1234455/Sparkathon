9a971f003aa9b384d95056c6946a2688301c0e7f
AkashDeep1234455 <akashdeep19735@gmail.com>
2024-08-10 19:28:30 +0530

    nav bar complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import NavBar from './NavBar'
 import "./style.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<NavBar/>,
+  },
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NavBar />
+     <RouterProvider router={router}/>
   </StrictMode>,
 )
