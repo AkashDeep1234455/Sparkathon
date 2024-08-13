ffc917cae16ac81d68d3db8e4dd49124ccefe70e
Aditya Singh <adityasingh1464@gmail.com>
2024-08-14 00:52:57 +0530

    added notification panel

---
+++ frontend/src/components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+import './Notification.css'
+
+export default function Notification(){
+    return (
+        <div className="Notification">
+            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam molestias eos asperiores magni in pariatur, esse tempore modi nobis nisi reprehenderit inventore optio, eius iste. Omnis vero beatae inventore a cupiditate pariatur asperiores consectetur blanditiis nobis nam ipsam quia earum aspernatur, culpa temporibus neque recusandae ratione aut quibusdam vitae ut! */}
+
+            <b>Message</b> : Lorem ipsum dolor sit amet consectetur adipisicing elit. A pariatur deleniti exercitationem incidunt non praesentium. Aspernatur corrupti illo eum omnis.
+        </div>
+    )
+}
