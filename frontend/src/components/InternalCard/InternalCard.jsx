2b3231a773ab71f28c28f0a941e4b5f35be74ece
Aditya Singh <adityasingh1464@gmail.com>
2024-08-15 16:00:17 +0530

    change date formate

--- frontend/src/components/InternalCard/InternalCard.jsx
+++ frontend/src/components/InternalCard/InternalCard.jsx
@@ -133,6 +133,13 @@ export default function InternalCard() {
     });
   };
 
+  const convertToIST = (dateString) => {
+    const utcDate = new Date(dateString);
+    const options = { timeZone: 'Asia/Kolkata', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    return utcDate.toLocaleString('en-IN', options);
+  };
+  
+
   return (
     <>
       <div>
@@ -149,7 +156,9 @@ export default function InternalCard() {
               <div className="InternalCard_mid">
                 <h2 className="InternalCard_name">{d.productName}</h2>
                 {sD[0].expiryDate ? (
-                  <span>Expiry Date : {sD[0].expiryDate}</span>
+                  <span>Expiry Date :
+                    {convertToIST(sD[0].expiryDate)}
+                  </span>
                 ): null}
                 <span>ProductId : {sD[0].productId}</span>
 
