2233f3f1522415b34b5d66017baa1a2a86b047b3
AkashDeep1234455 <akashdeep19735@gmail.com>
2024-08-15 15:06:07 +0530

    search panel fixed successfully

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import './Filter.css'
 
 export default function Filter(){
+    const [checkStatus,setCheckStatus] = useState(false);
+    const checkBoxChangeHandler = ()=>{
+        setCheckStatus(!checkStatus);
+        console.log(checkStatus);
+    }
+
+
     return(
         <div className="Filter">
-            <label> <input type="checkbox"/> : Expiry Date</label>
+            <label> <input type="checkbox" onChange={checkBoxChangeHandler}/> : Expiry Date</label>
         </div>
     )
 }
